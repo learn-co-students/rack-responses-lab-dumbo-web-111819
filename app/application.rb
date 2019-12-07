f82a554acbbdd7edf40679a64a89b4f4c1d7c744
Annie <anniezheng.anniee@gmail.com>
2019-12-07 18:37:13 -0500

    Done.

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
         t = Time.now
 
 
-        if t.hour > 12 
+        if t.hour >= 12 
             resp.write "Good Afternoon!"
         else
             resp.write "Good Morning!"
