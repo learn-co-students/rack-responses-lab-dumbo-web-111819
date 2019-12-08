19ccc12557779114ec1274e38df4a62c2032f388
Baruch Phillips <baruchphillips@gmail.com>
2019-12-08 00:12:33 -0500

    Done.

---
+++ app/application.rb
@@ -0,0 +1,19 @@
+class Application
+ 
+    def call(env)
+      resp = Rack::Response.new
+    now = Time.now
+    if now.hour < 12 
+        resp.write "Good morning"
+    else 
+        resp.write "Good afternoon"
+    end 
+      resp.finish
+  
+end 
+end 
+
+
+
+
+# if before noon "Good morning" if after Good afternoon
